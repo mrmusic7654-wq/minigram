412e4942ca89ca703833a2188dc219c688f510e6
mrmusic7654-wq <mr.music7654@gmail.com>
2025-12-29 08:10:09 +0545

    Refactor theme provider to use AppThemeMode

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:minigram/theme/theme_manager.dart';
 
 class ThemeProvider with ChangeNotifier {
-  ThemeMode _themeMode = ThemeMode.gold;
-
-  ThemeMode get themeMode => _themeMode;
-
-  void setTheme(ThemeMode theme) {
-    _themeMode = theme;
+  AppThemeMode _themeMode = AppThemeMode.gold;
+  
+  AppThemeMode get themeMode => _themeMode;
+  
+  // Get Flutter's ThemeMode for MaterialApp
+  ThemeMode get flutterThemeMode {
+    switch (_themeMode) {
+      case AppThemeMode.light:
+        return ThemeMode.light;
+      case AppThemeMode.dark:
+        return ThemeMode.dark;
+      case AppThemeMode.gold:
+        return ThemeMode.dark; // Gold theme uses dark base
+    }
+  }
+  
+  ThemeData get currentTheme => ThemeManager.getThemeData(_themeMode);
+  
+  void setThemeMode(AppThemeMode mode) {
+    _themeMode = mode;
+    ThemeManager.setThemeMode(mode);
     notifyListeners();
   }
-
+  
   void toggleTheme() {
-    _themeMode = ThemeMode.values[(_themeMode.index + 1) % ThemeMode.values.length];
+    switch (_themeMode) {
+      case AppThemeMode.light:
+        _themeMode = AppThemeMode.dark;
+        break;
+      case AppThemeMode.dark:
+        _themeMode = AppThemeMode.gold;
+        break;
+      case AppThemeMode.gold:
+        _themeMode = AppThemeMode.light;
+        break;
+    }
+    ThemeManager.setThemeMode(_themeMode);
     notifyListeners();
   }
 }
